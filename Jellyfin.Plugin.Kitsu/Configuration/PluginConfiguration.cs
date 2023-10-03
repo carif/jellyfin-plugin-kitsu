f870d0b93ecb9ae0356ef04446771a13d6ccef64
Patrick Farwick <9168045+carif@users.noreply.github.com>
2023-10-03 14:44:55 +0200

    Add documentation to parts of the code-base

--- Jellyfin.Plugin.Kitsu/Configuration/PluginConfiguration.cs
+++ Jellyfin.Plugin.Kitsu/Configuration/PluginConfiguration.cs
@@ -2,6 +2,9 @@ using MediaBrowser.Model.Plugins;
 
 namespace Jellyfin.Plugin.Kitsu.Configuration
 {
+    /// <summary>
+    /// Plugin configuration class for Kitsu library.
+    /// </summary>
     public class PluginConfiguration : BasePluginConfiguration
     {
         // store configurable settings your plugin might need
--- Jellyfin.Plugin.Kitsu/Plugin.cs
+++ Jellyfin.Plugin.Kitsu/Plugin.cs
@@ -8,19 +8,36 @@ using MediaBrowser.Model.Serialization;
 
 namespace Jellyfin.Plugin.Kitsu
 {
+    /// <summary>
+    /// Plugin class for the Kitsu library.
+    /// </summary>
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        public override string Name => "Kitsu";
-
-        public override Guid Id => Guid.Parse("88d809e2-056b-47f4-9911-073949b9963f");
-
+        /// <summary>
+        /// Initializes a new instance of the &lt;see cref="Plugin"/&gt; class.
+        /// </summary>
+        /// <param name="applicationPaths">application paths</param>
+        /// <param name="xmlSerializer">xml serializer</param>
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
         }
 
+        /// <summary>
+        /// Gets the instance of Kitsu plugin.
+        /// </summary>
         public static Plugin Instance { get; private set; }
 
+        /// <inheritdoc/>
+        public override string Name => "Kitsu";
+
+        /// <inheritdoc/>
+        public override Guid Id => Guid.Parse("88d809e2-056b-47f4-9911-073949b9963f");
+
+        /// <summary>
+        /// Return the plugin configuration page.
+        /// </summary>
+        /// <returns>PluginPageInfo.</returns>
         public IEnumerable<PluginPageInfo> GetPages()
         {
             return new[]
@@ -28,7 +45,7 @@ namespace Jellyfin.Plugin.Kitsu
                 new PluginPageInfo
                 {
                     Name = this.Name,
-                    EmbeddedResourcePath = string.Format("{0}.Configuration.configPage.html", GetType().Namespace)
+                    EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.html"
                 }
             };
         }
